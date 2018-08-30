c7bcca67bb7fe2950c1e29ca671f198ff3f9a477
Thirunavukkarasu <tamilan.arasu@gmail.com>
2018-08-31 03:15:33 +0530

    added sass modules for better style management

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./assets/css/animate.min.css";
+import "./assets/sass/mern-starter-app.css?v=1.0.0";
 import './index.css';
 import Application from './Application';
 
